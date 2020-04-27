612273b55e2e21b8c95b5557181ad0172b03bec5
Kevin <53408605+Schmevlin@users.noreply.github.com>
2020-04-27 15:52:56 -0400

    SubBoard basically done

---
+++ Board.java
@@ -0,0 +1,17 @@
+/**
+ * Class for outer "ultimate" board
+ * 
+ * @version 4/27/20
+ * @author Kevin Harris
+ */
+public class Board{
+    private SubBoard[][] spaces;
+
+    public Board(){
+        for (int y = 0; y < spaces.length; y++) {
+            for (int x = 0; x < spaces[0].length; x++) {
+                spaces[y][x] = new SubBoard();
+            }
+        }
+    }
+}
