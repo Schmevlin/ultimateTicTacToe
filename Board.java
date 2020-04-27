1b7df3592be66397cf38bbdc47386d15f3958964
Kevin <53408605+Schmevlin@users.noreply.github.com>
2020-04-27 15:55:03 -0400

    forgot how arrays work

--- Board.java
+++ Board.java
@@ -5,12 +5,14 @@
  * @author Kevin Harris
  */
 public class Board{
-    private SubBoard[][] spaces;
+    private SubBoard[][] spaces = new SubBoard[3][3];
+    private char[][] wonBoards = new char[3][3];
 
     public Board(){
         for (int y = 0; y < spaces.length; y++) {
             for (int x = 0; x < spaces[0].length; x++) {
                 spaces[y][x] = new SubBoard();
+
             }
         }
     }
