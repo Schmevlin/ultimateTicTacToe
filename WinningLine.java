d38869afb168ebc8b5d15f7ed43c8a4d6007e9a3
andb3 <andb3003@yahoo.com>
2020-05-04 15:13:04 -0400

    - added javadocs for WinningLine and Pair

--- WinningLine.java
+++ WinningLine.java
@@ -1,4 +1,10 @@
-
+/**
+ * Class to represent any three-in-a-row spaces on a SubBoard. 
+ * Holds both the position of the line and the player that won the line (either 'x' or 'o'). 
+ * Can also represent no line through {@link Line#NONE} and ' ' as winner.
+ * @author Andy Burris and Kevin Harris
+ * @version 4 May 2020
+ */
 public class WinningLine {
     private Line line;
     private char winner;
@@ -8,14 +14,24 @@ public class WinningLine {
         this.winner = winner;
     }
 
+    /**
+     * @return winner that this represents (either 'x', 'o', or ' ')
+     */
     public char getWinner(){
         return winner;
     }
 
+    /**
+     * @return {@link Line} that this represents (any of the values of Line)
+     */
     public Line getLine(){
         return line;
     }
 
+    /**
+     * Utility method to check if this line represents a winner
+     * @return false if line is {@link Line#NONE} or char is ' ', true otherwise
+     */
     public boolean hasWinner(){
         return line != Line.NONE && winner != ' ';
     }
--- util/Pair.java
+++ util/Pair.java
@@ -1,5 +1,12 @@
 package util;
 
+/**
+ * Generic class to represent two objects
+ * @param <A> Type of first object
+ * @param <B> Type of second object
+ * @author Andy Burris
+ * @version 4 May 2020
+ */
 public class Pair<A, B> {
 
     public A first;
